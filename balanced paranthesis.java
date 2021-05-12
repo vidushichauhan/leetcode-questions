c13c2b1ab340e215a291925617eda413875391ec
vidushichauhan <vidushic0@gmail.com>
2021-05-12 21:43:49 +0530

    valid paranthesis

---
+++ balanced paranthesis.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public boolean isValid(String s) {
+      boolean x=false;  
+        Deque<Character>d=new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+           //if(d.isEmpty())
+        
+           if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+               d.push(s.charAt(i));
+           }
+         if(!d.isEmpty()){
+            if(s.charAt(i)==')' && d.peek()=='('){d.pop();}
+           else if(s.charAt(i)=='}' && d.peek()=='{'){d.pop();}
+            else if(s.charAt(i)==']' && d.peek()=='['){d.pop();}
+            else if(s.charAt(i)==')' && d.peek()!='('){return false;}
+           else if(s.charAt(i)=='}' && d.peek()!='{'){return false;}
+            else if(s.charAt(i)==']' && d.peek()!='['){return false;}
+         Thank you Rohan...so proud of you....you are sooo smart....wowowo
+         }//(
+            
+            else{return false;}
+        }
+        
+        if(d.isEmpty()){x=true;}
+        return x;
+    }  
+}
